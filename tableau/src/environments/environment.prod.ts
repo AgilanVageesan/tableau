d3762aaecbddcef5ececad087065c09cc812c047
Agilan Vageesan <agilanvageesan@gmail.com>
2021-06-07 01:06:28 +0530

    prod env

--- tableau/src/environments/environment.prod.ts
+++ tableau/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  API_BASE_URL: "https://api.themoviedb.org/3/",
+  TMDB_API_KEY: "a1ecd2d1a6b12f46396e32f012ff00af",
+  API_LANGUAGE: "en-US",
 };
